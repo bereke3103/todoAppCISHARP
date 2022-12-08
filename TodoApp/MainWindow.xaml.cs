e6b6e32ee0b3d51820d967e86cfdcdd9637fd726
bereke <bereke3103@mail.ru>
2022-12-08 13:50:43 +0600

    check

---
+++ TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TodoApp.Models;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<TodoModel> _todoDataList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _todoDataList = new BindingList<TodoModel>()
+            {
+                new TodoModel() {Text = "sadas"},
+                new TodoModel() {Text = "abc"},
+            };
+            dgTodoList.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;
+        }
+
+        private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            switch (e)
+        }
+    }
+}
---
+++ TodoApp/Models/TodoModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    class TodoModel
+    {
+
+        private bool _isDone;
+        private string _text;
+
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+         
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+
+
+    }
+}
